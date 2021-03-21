1c08102309f8ef4e8b7f3533f10bb14512086ec4
Isacco <isacco.sordo@gmail.com>
2021-03-21 20:57:38 +0100

    aggiunto commenti

--- src/main/java/com/hotmokafe/application/blockchain/CreateAccount.java
+++ src/main/java/com/hotmokafe/application/blockchain/CreateAccount.java
@@ -39,32 +39,29 @@ public class CreateAccount {
         String result = "errore";
 
         TendermintBlockchainConfig config = new TendermintBlockchainConfig.Builder().build();
-        ConsensusParams consensus = null;
+        ConsensusParams consensus;
         try {
             consensus = new ConsensusParams.Builder().build();
         } catch (NoSuchAlgorithmException e) {
             return result;
         }
 
-        // the path of the packaged runtime Takamaka classes
         Path takamakaCodePath = Paths.get
                 ("modules/io-takamaka-code-1.0.0.jar");
 
-        // the path of the user jar to install
         Path familyPath = Paths.get("modules/family_exported-0.0.1-SNAPSHOT.jar");
 
         try (Node node = TendermintBlockchain.init(config, consensus)) {
-            // first view: store io-takamaka-code-1.0.0.jar and create manifest and gamete
+            // 1) creo manifest e gamete
             InitializedNode initialized = InitializedNode.of
                     (node, consensus, takamakaCodePath, GREEN_AMOUNT, RED_AMOUNT);
 
-            // second view: store family-0.0.1-SNAPSHOT.jar: the gamete will pay for that
+            // 2) the gamete andrà a pagare per la transazione con unità di "gas"
             NodeWithJars nodeWithJars = NodeWithJars.of
                     (node, initialized.gamete(), initialized.keysOfGamete().getPrivate(),
                             familyPath);
 
-            // third view: create two accounts, the first with 10,000,000 units of green coin
-            // and the second with 20,000,000 units of green coin
+            // 3) Assegno delle "monete" agli account per poter pagare le transazioni
             NodeWithAccounts nodeWithAccounts = NodeWithAccounts.of
                     (node, initialized.gamete(), initialized.keysOfGamete().getPrivate(),
                             BigInteger.valueOf(10_000_000), BigInteger.valueOf(20_000_000));
@@ -75,70 +72,62 @@ public class CreateAccount {
             StorageReference personStorage = node.addConstructorCallTransaction
                     (new ConstructorCallTransactionRequest(
 
-                            // signer on behalf of the first account
+                            // request da parte del primo account
                             SignedTransactionRequest.Signer.with(node.getSignatureAlgorithmForRequests(),
                                     nodeWithAccounts.privateKey(0)),
 
-                            // the first account pays for the transaction
+                            // il primo account paga la request
                             nodeWithAccounts.account(0),
 
-                            // nonce: we know this is the first transaction
-                            // with nodeWithAccounts.account(0)
                             ZERO,
 
-                            // chain identifier
                             "",
 
-                            // gas provided to the transaction
+                            // gas usato per pagare la transazione
                             BigInteger.valueOf(10_000),
 
-                            // gas price
+                            // il costo del gas per transazione
                             panarea(gasHelper.getSafeGasPrice()),
 
-                            // reference to family-0.0.1-SNAPSHOT.jar
-                            // and its dependency io-takamaka-code-1.0.0.jar
                             nodeWithJars.jar(0),
 
-                            // constructor Person(String,int,int,int)
+                            // Person(String,int,int,int)
                             new ConstructorSignature(PERSON, ClassType.STRING, INT, INT, INT),
 
-                            // parametri
+                            // parametri attuali
                             new StringValue(person.getName()), new IntValue(person.getDay()),
                             new IntValue(person.getMonth()), new IntValue(person.getYear())
                     ));
 
             StorageValue s = node.addInstanceMethodCallTransaction(new InstanceMethodCallTransactionRequest(
 
-                    // signer on behalf of the second account
+                    // request da parte del secondo account per ottenere una response dalla blockchain
                     SignedTransactionRequest.Signer.with(node.getSignatureAlgorithmForRequests(), nodeWithAccounts.privateKey(1)),
 
-                    // the second account pays for the transaction
+                    // il primo account paga la response
                     nodeWithAccounts.account(1),
 
-                    // nonce: we know this is the first transaction
-                    // with nodeWithAccounts.account(1)
                     ZERO,
 
-                    // chain identifier
                     "",
 
-                    // gas provided to the transaction
+                    // gas usato per pagare la transazione
                     BigInteger.valueOf(10_000),
 
-                    // gas price
+                    // il costo del gas per transazione
                     panarea(gasHelper.getSafeGasPrice()),
 
-                    // reference to family-0.0.1-SNAPSHOT.jar
-                    // and its dependency io-takamaka-code-1.0.0.jar
                     nodeWithJars.jar(0),
 
-                    // method to call: String Person.toString()
+                    // chiamo il toString()
                     new NonVoidMethodSignature(PERSON, "toString", ClassType.STRING),
 
-                    // receiver of the method to
+                    // ripongo la response in uno storage per usi futuri
                     personStorage
             ));
+
             result = "Operazione terminata con successo. Account: " + s.toString() + " creato correttamente";
+
         } catch (Exception e) {
             e.printStackTrace();
         }
